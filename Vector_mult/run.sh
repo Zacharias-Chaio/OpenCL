4a83865f5359551400e55a48c2cab48e08532504
jiaolh <Longhui.Jiao@myorbita.net>
2021-11-11 10:11:52 +0800

    Delete useless Makefile or config files

--- Vector_mult/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export VIVANTE_SDK_DIR=../vip_driver/sdk
-export VIV_VX_ENABLE_SAVE_NETWORK_BINARY=1
-export LD_LIBRARY_PATH=/usr/lib/arm-linux-gnueabihf/lapack:/usr/lib/arm-linux-gnueabihf:/lib/arm-linux-gnueabihf:/usr/lib:/lib:../vip_driver/sdk/drivers:/usr/lib/arm-linux-gnueabihf/blas/
-export VSI_USE_IMAGE_PROCESS=1
-#export VNN_LOOP_TIME=300
-export VIVANTE_SDK_DIR=../vip_driver/sdk
-./findmax
